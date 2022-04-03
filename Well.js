d156f945a85e1edebac5b4894e24b4c98d91ea48
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-04-02 21:52:23 -0700

    new changes

--- Well.js
+++ Well.js
@@ -1,82 +1,122 @@
-const quizData = [
+// Quiz Questions
+const dietData = [
     {
-      question: "How much exercise should you get in a day?",
-      a: "30 min",
-      b: "1 hour",
-      c: "None",
-      d: "1+ hours",
-      correct: "a", // https://www.mayoclinic.org/healthy-lifestyle/fitness/expert-answers/exercise/faq-20057916
-    }, 
+      question: "1. What are the percentages of Americans that are obese and lacking in nutrients, respectively.",
+      a: "33% / 80%",
+      b: "10% / & 55%",
+      c: "15% / 45%",
+      d: "25% / 50%",
+      correct: "b", 
+    },
+  //https://www.npr.org/sections/thesalt/2016/08/03/487640479/75-percent-of-americans-say-they-eat-healthy-despite-evidence-to-the-contrary#:~:text=Press-,75%20Percent%20of%20Americans%20Say%20They%20Eat%20Healthy%20%E2%80%94%20Despite%20Evidence,we%20eat%20with%20blinders%20on.
     {
-      question: "What percentage of your meal should be fruits and vegetables?",
-      a: "25$",
-      b: "None",
-      c: "100%",
-      d: "50%",
-      correct: "d",  // https://med.libretexts.org/Courses/Sierra_College/Sierra%3A_NUTF10_(Teh)/02%3A_Achieving_a_Healthy_Diet/2.06%3A_My_Plate_-_A_Guide_for_Eating_More_Healthfully
-    }, 
-      {
-      question: "What percentage of your meal should be grains and protein?",
-      a: "50%",
-      b: "25%",
-      c: "75%",
-      d: "15%",
-      correct: "a", // https://med.libretexts.org/Courses/Sierra_College/Sierra%3A_NUTF10_(Teh)/02%3A_Achieving_a_Healthy_Diet/2.06%3A_My_Plate_-_A_Guide_for_Eating_More_Healthfully
-    }, 
-      {
-      question: "How much water should you be drinking a day?",
-      a: "1 Gallon (Men) 0.8 Gallon (Women)",
-      b: "18.5 Cups (Men) and 14.5 Cups (Women)",
-      c: "15.5 Cups (Men) and 11.5 Cups (Women)",
-      d: "24.0 Cups (Men) and 20.0 Cups (Women)",
-      correct: c, // https://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/water/art-20044256 
-    }, 
-      {
-      question: "Should your diet consist of green vegetables?",
-      a: "Yes",
-      b: "I am not sure",
-      c: "No",
-      d: "Depends on what your doctor says.",
-      correct: "a", 
-    }, 
-      {
-      question: "What does a healthy diet look to you?",
-      a: "Apples, Bananas, Chicken, Dark Chocolate, Rice, Brocolli",
-      b: "Milk, Whole Grain Cereal, Oranges, Beef, Hamburger, Lettuce",
-      c: "Pasta, Garlic Bread, Pizza, House Salad with Dressing, Smoothie",
-      d: "ALL OF THE ABOVE",
+      question: "2. From what food group should you eat the most amount of servings daily?",
+      a: "Grains",
+      b: "Vegetables",
+      c: "Protein",
+      d: "Grains & Vegetables",
       correct: "d", 
+    },
+  //https://healthyeating.sfgate.com/food-group-needed-highest-proportion-day-3063.html
+    {
+      question: "3. Which one is not a sign of a bad diet?",
+      a: "Digestive Discomfort",
+      b: "Feeling Tired",
+      c: "Bad Oral Health",
+      d: "All of the Above",
+      correct: "d", 
+    },
+  //https://health.clevelandclinic.org/7-sneaky-signs-of-an-unhealthy-diet/
+    {
+      question: "4. What additional factor is not necessary in a balanced diet?",
+      a: "Healthy Oils",
+      b: "Exercise",
+      c: "Water",
+      d: "Sugar",
+      correct: "d", 
+    }, //https://www.health.harvard.edu/heart-health/the-sweet-danger-of-sugar
+    {  
+      question: "5. Eating smaller meals more often is better than the standard 3 meals", 
+      a: "True",
+      b: "False",
+      c: "-",
+      d: "-",
+      correct: "b",
+      },
+  //https://www.heartandstroke.ca/healthy-living/healthy-eating/healthy-eating-basics
+    {
+      question: "6. Which one is not an example of processed food?", 
+      a: "White Rice",
+      b: "Bacon",
+      c: "White Meats",
+      d: "White Bread",
+      correct: "c",
+      },
+    {
+      question: "7. Which of the following are methods that can improve our access to healthier foods?",
+      a: "Healthier consumer advertisements/support",
+      b: "Increasing amount of healthier food chains",
+      c: "a & b",
+      d: "purchasing more high sodium products",
+      correct: "c",
+    },
+  //https://www.cdc.gov/nutrition/healthy-food-environments/improving-access-to-healthier-food.html
+    {
+     question: "8. True or False: You and your community can make a difference in pursuing health mandates",
+      a: "True",
+      b: "False",
+      c: "-",
+      d: "-",
+      correct: "a",
     }, 
-      {
-      question: "",
-      a: "",
-      b: "",
-      c: "",
-      d: "",
-      correct: , 
-    }, 
-      {
-      question: "",
-      a: "",
-      b: "",
-      c: "",
-      d: "",
-      correct: , 
-    }, 
-      {
-      question: "",
-      a: "",
-      b: "",
-      c: "",
-      d: "",
-      correct: , 
-    }, 
-      {
-      question: "",
-      a: "",
-      b: "",
-      c: "",
-      d: "",
-      correct: , 
-    }, 
-  ]
+    {
+      question: "9. What is the cost of a healthy diet in proportion to an unhealthy diet?",
+      a: "$1.50 daily increase",
+      b: "$.75 daily decrease",
+      c: "$3.00 daily decrease",
+      d: "$5.00 daily increase",
+      correct: "a",
+    },
+  //https://www.hsph.harvard.edu/news/press-releases/healthy-vs-unhealthy-diet-costs-1-50-more/
+    {
+      question: "10. Are your emotions, height and performance hindered by a bad diet?",
+    a: "yes",
+    b: "no",
+    c: "-",
+    d: "-",
+    correct: "a",
+    }
+  //https://www.health.harvard.edu/blog/nutritional-psychiatry-your-brain-on-food-201511168626
+  ];
+
+  const quiz = document.getElementById("quiz");
+  const answerEls = document.querySelectorAll(".answer");
+  const theQuestion = document.getElementById("question");
+  const choice_a = document.getElementById('choice_a');
+  const choice_b = document.getElementById('choice_b');
+  const choice_c = document.getElementById('choice_c');
+  const choice_d = document.getElementById('choice_d');
+  const nextButton = document.getElementById("next");
+
+  let currentQuestion = 0; // Initialized the question counter to change from one to question to the next
+  let score = 0;  // Initialized the Score counter 
+
+  setQuiz();
+
+  function setQuiz() {
+    const currentQuestionSet = quizSet[currentQuestion];
+    
+    theQuestion.innerText = currentQuestionSet.question;
+    choice_a.innerText = currentQuestionSet.a;
+    choice_b.innerText = currentQuestionSet.b;
+    choice_c.innerText = currentQuestionSet.c;
+    choice_d.innerText = currentQuestionSet.d;
+
+    currentQuestion++;
+  }
+
+    
+
+
+
+
